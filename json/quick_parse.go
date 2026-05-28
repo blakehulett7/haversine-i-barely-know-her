7b1a1b594047a91ca19d9528e0cfd3b66cbb1b4a
blakehulett7 <blake.hulett7@gmail.com>
2026-05-28 14:09:25 -0500

    valid chunks maybe

--- json/quick_parse.go
+++ json/quick_parse.go
@@ -9,15 +9,21 @@ import (
 )
 
 const row_size = 4
+const threads = 10
 
 func QuickParse(data []byte, size int) []models.Row {
 	start := metrics.Start(metrics.QuickParse)
 	defer metrics.End(start, uint64(len(data)), metrics.QuickParse)
 
 	rows := make([]models.Row, size)
-
 	s := string(data)
 	runes := []rune(s)
+
+	chunks := chunk_runes(runes)
+	fmt.Println(string(chunks[9]))
+	fmt.Println(len(runes))
+	fmt.Println(len(chunks[9]))
+
 	cursor := find_next(runes, 0, ':')
 	cursor++
 
@@ -66,4 +72,26 @@ func parse_row(runes []rune, cursor int) (models.Row, int) {
 		X1: floats[2],
 		Y1: floats[3],
 	}, cursor
+}
+
+func chunk_runes(runes []rune) [threads][]rune {
+	var chunks [threads][]rune
+	size := len(runes) / threads
+
+	for i := range threads {
+		end := size * (i + 1)
+		end = find_next(runes, end, '}')
+		chunks[i] = runes[size*i : end+1]
+	}
+
+	return chunks
+}
+
+func print_runes(runes []rune) {
+	fmt.Print("[")
+	for _, r := range runes {
+		fmt.Printf("%q ", r)
+	}
+	fmt.Println()
+	fmt.Println()
 }
